d8de493a2ef16f3206b58cdb303cc6b539cb159d
Luiz Henrique <luiz.henrique@moneyex.io>
2019-02-08 01:20:39 -0300

    Creation of the first mongodb module through the node app

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 const app = express();
 
+mongoose.connect('my-mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+
+requireDir("./src/models");
+const Product = mongoose.model('Product');
+
 app.get('/', (request, response) => {
+    Product.create({
+        title: "bla 1",
+        description:  "bla 2",
+        url:  "bla 3",
+    });
     response.send('Hello, yo!');
     console.log(request, response);
 });
+
 
 app.listen(3001);
 
